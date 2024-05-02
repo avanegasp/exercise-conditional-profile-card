2d29f4979350642d6a6234841f0cecafd0d1fe1f
Angie Vanegas P <arco527@gmail.com>
2024-05-02 18:39:46 +0000

    Add variables.name - variables.lastname

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,13 @@ import "../style/index.css";
 
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
+ *  This function is called every time the user changes types or 
+ *  changes any input
  * 
     {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
+        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's 
+        url that will be used as a background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
         socialMediaPosition: "right", // social media bar position (left or right)
         
@@ -30,11 +32,19 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
+  console.log(variables);
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
+          
+          <h1>
+            ${variables.name ? variables.name : "Caro"} 
+            ${variables.lastName ? variables.lastName : "Rios"}
+          </h1>
+
+          
+          <h2>${variables.role}</h2>
+          
           <h3>Miami, USA</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
